9bc0bab618810ec6da2df6e31c7f4bb0cbf394a0
Kyle Henry <kylehenry003@gmail.com>
2019-11-20 22:09:57 -0500

    Updated php file

--- world.php
+++ world.php
@@ -7,12 +7,36 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
 $stmt = $conn->query("SELECT * FROM countries");
+$stmt2 = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%' ");
+
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 
+echo '<table style="border-bottom: 1px dotted #000000;">
+<tr>
+    <td>'."Country Name".'</td>
+    <td>'."Continent".'</td>
+    <td>'."Independence Year".'</td>
+    <td>'."Head of State".'</td>
+</tr>';
 
-?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+if($country == ""){
+    foreach ($results as $row) {
+      echo '<tr style="border-bottom: 1px dotted #000000;">
+          <td>'.$row['name']. '</td>
+          <td>'.$row['continent']. '</td>
+          <td>'.$row['independence_year']. '</td>
+          <td>'.$row['head_of_state']. '</td></tr>';
+    }
+          
+}
+else {
+  foreach ($results2 as $row){
+    echo '<tr><td>'.$row['name'].'</td>
+          <td>'.$row['continent']. '</td>
+          <td>'.$row['independence_year']. '</td>
+          <td>'.$row['head_of_state']. '</td></tr><br>';
+  }
+}
+echo '</table>';
+
